b9f7613ef532c504c143760cb251699d068aa426
DragonSlayer <rjkee13@gmail.com>
2024-09-05 20:04:36 -0400

    Init Commit Shows the code for question 1 of lab 0

---
+++ Lab 0/driver.java
@@ -0,0 +1,16 @@
+/*
+ * Use a table to represtent Test Suite
+ * Columns: | Type | Input(s) | Expected Output | Actual Output | Pass? |
+ *  
+ */
+
+public class driver {
+    public static void main(String[] args) {
+        System.out.println("coding is the best \ndon't let it make you feel dumb \nkeep calm, debug on." + "\n");
+        System.out.println("chaos reigns within \nreflect, repent, and reboot \norder shall return." + "\n");
+        System.out.println("code belongs to us \nand never only to me \ncollaboration." + "\n");
+        System.out.println("make it work make it \nreadable make it effic \nient." + "\n");
+        System.out.println("what is with this code? \noh my, looks like I wrote it \nwhat was I thinking?" + "\n");
+        System.out.println("Programming is fun \nonly if you're good at it \notherwise it sucks" + "\n");
+    }
+}
