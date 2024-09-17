3f2d7b53d6266445528a9c44ccdf66be0174f824
DragonSlayer <rjkee13@gmail.com>
2024-09-17 18:18:47 -0400

    Fixed Initials.java

--- Lab 1/Initials.java
+++ Lab 1/Initials.java
@@ -2,7 +2,7 @@ public class Initials {
     public static void main(String[] args) {
 
         System.out.println(
-                " ______   |  /  \n|      |  | /   \n|______|  |/    \n|\\        |\\    \n| \\       | \\  \n|  \\      |  \\");
+                " ______   |    /  \n|      |  |   /   \n|______|  |__/    \n|\\        |  \\    \n| \\       |   \\  \n|  \\      |    \\");
 
     }
 }
